a3eda99a7d4f803abaa6b65ca130429b8316dec5
ByteDecoder <rreyes@canaryhealth.com>
2020-08-30 01:53:54 -0500

    Game scale added + level map rendered

--- src/js/main.ts
+++ src/js/main.ts
@@ -6,31 +6,34 @@ const { canvas } = init();
 const submarineSprite = new Image();
 submarineSprite.src = '../sprites/submarine.png';
 
+const gameScale = 4;
+const levelWidthSize = 8;
+const levelHeightSize = 5;
+
 const world = [
   [1, 1, 1, 1, 1, 1, 1, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
 const submarine = Sprite({
-  x: 100,
-  y: 80,
+  x: 16 * gameScale,
+  y: 16 * gameScale,
   color: 'red',
-  width: 40,
-  height: 40,
-  dx: 2
+  width: 16 * gameScale,
+  height: 16 * gameScale
 });
 
-const wallFactory = (x, y) =>
+const wallFactory = (x: number, y: number, scale = 1) =>
   Sprite({
-    x,
-    y,
-    color: 'red',
-    width: 40,
-    height: 40,
-    dx: 2
+    x: x * scale,
+    y: y * scale,
+    color: 'green',
+    width: 16 * scale,
+    height: 16 * scale,
+    name: `wall_${x}_${y}`
   });
 
 const text = Text({
@@ -43,21 +46,39 @@ const text = Text({
   textAlign: 'center'
 });
 
-const worldRenderer = () => {
-  for(let x=0, x< 5; x++) {
+let levelMap = [];
 
+const worldRenderer = () => {
+  const level = [];
+  for (let y = 0; y < levelHeightSize; y += 1) {
+    for (let x = 0; x < levelWidthSize; x += 1) {
+      if (world[y][x] === 1) {
+        const wall = wallFactory(x * 16, y * 16, gameScale);
+        level.push(wall);
+      }
+    }
   }
+  return level;
 };
+
+levelMap = worldRenderer();
 
 const loop = GameLoop({
   update(dt) {
     submarine.update();
 
     if (keyPressed('up')) {
-      submarine.y -= 4 + dt;
+      submarine.y -= 2 + dt;
     }
     if (keyPressed('down')) {
-      submarine.y += 4 + dt;
+      submarine.y += 2 + dt;
+    }
+
+    if (keyPressed('right')) {
+      submarine.x += 2 + dt;
+    }
+    if (keyPressed('left')) {
+      submarine.x -= 2 + dt;
     }
 
     if (submarine.x > canvas.width) {
@@ -66,6 +87,7 @@ const loop = GameLoop({
   },
   render() {
     text.render();
+    levelMap.forEach((block) => block.render());
     submarine.render();
   }
 });
