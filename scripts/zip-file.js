9e74d38ab03d7f8dde2ebab5ca9f2ee23a3ce3c7
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-08-31 15:34:37 -0500

    Build, zip game and bundle game zip size check under 13kb added

--- scripts/zip-file.js
+++ scripts/zip-file.js
@@ -1,11 +1,53 @@
-const fs = require('fs');
-const archiver = require('archiver');
+var fs = require('fs');
+var archiver = require('archiver');
 
 const distDir = process.cwd() + '/dist';
 const output = fs.createWriteStream(distDir + '/zipped/game.zip');
 const archive = archiver('zip', { zlib: { level: 9 } });
 
-archive.pipe(output);
-archive.file(distDir + '/index.html', { name: 'index.html' });
+// listen for all archive data to be written
+// 'close' event is fired only when a file descriptor is involved
+output.on('close', function () {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
 
-archive.finalize();
+// This event is fired when the data source is drained no matter what was the data source.
+// It is not part of this library but rather from the NodeJS Stream API.
+// @see: https://nodejs.org/api/stream.html#stream_event_end
+output.on('end', function () {
+  console.log('Data has been drained');
+});
+
+// good practice to catch warnings (ie stat failures and other non-blocking errors)
+archive.on('warning', function (err) {
+  if (err.code === 'ENOENT') {
+    // log warning
+  } else {
+    // throw error
+    throw err;
+  }
+});
+
+// good practice to catch this error explicitly
+archive.on('error', function (err) {
+  throw err;
+});
+
+fs.readdir(distDir, function (err, files) {
+  //handling error
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+
+  archive.pipe(output);
+
+  files.forEach(function (file) {
+    if (file.includes('.')) {
+      console.log(`Adding ${file} in the game zipfile`);
+      archive.file(`${distDir}/${file}`, { name: file });
+    }
+  });
+
+  archive.finalize();
+});
