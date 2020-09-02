7acf93089aa31427c9617902eb1ae76824d65a3e
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-09-02 13:16:09 -0500

    Final game title added

--- src/js/scenes/menuScene.ts
+++ src/js/scenes/menuScene.ts
@@ -1,9 +1,9 @@
 import { keyPressed } from 'kontra';
 import { createScene, IGameScene } from './gameScene';
-import { createSmText, createMdText, createLgText } from '../utils/textUtil';
+import { createSmText, createMdText, createLgText, createText } from '../utils/textUtil';
 import Game from '../gameEngine/game';
 
-const gameTitle = createLgText('404 Dark Room', { x: 400, y: 200 });
+const gameTitle = createLgText('404 MISSION', { x: 400, y: 200 });
 
 const gameSubtitle = createMdText('An underwater adventure', {
   x: 400,
@@ -11,8 +11,9 @@ const gameSubtitle = createMdText('An underwater adventure', {
 });
 
 const gameAction = createSmText('Press [ENTER] to continue', { x: 400, y: 400 });
+const gameAuthor = createText('Made by ByteDecoder, 2020', { x: 170, y: 580 }, 16);
 
-const props = [gameTitle, gameSubtitle, gameAction];
+const props = [gameTitle, gameSubtitle, gameAction, gameAuthor];
 
 const update = () => {
   if (keyPressed('enter')) {
