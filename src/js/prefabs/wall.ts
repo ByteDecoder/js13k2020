265311e3dd03716d55cd7c8087dbd2da7a77d7b6
Rodrigo Reyes <rreyes@canaryhealth.com>
2020-09-03 14:53:21 -0500

    Adding wall sprite in the game

--- src/js/prefabs/wall.ts
+++ src/js/prefabs/wall.ts
@@ -1,21 +1,16 @@
 import { Sprite } from 'kontra';
-import { blockSize, gameScale } from '../gameGlobals';
+import { gameScale } from '../gameGlobals';
 import Game from '../gameEngine/game';
 
 /**
  * Creates a wall prefab.
  * @param x
  * @param y
- * @param scale
  */
 const createWall = (x: number, y: number): Sprite =>
   Sprite({
     x: x * gameScale,
     y: y * gameScale,
-    color: 'green',
-    width: blockSize * gameScale,
-    height: blockSize * gameScale,
-    collitionIdx: `wall_${x}_${y}`,
     image: Game.getInstance().imageAssets.get('wall')
   });
 
