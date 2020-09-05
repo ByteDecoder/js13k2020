abc17225f88772e3a58ad6910ccc474535a0eb73
ByteDecoder <rreyes@canaryhealth.com>
2020-09-04 20:50:54 -0500

    Game text updated

--- src/js/scenes/missionCompletedScene.ts
+++ src/js/scenes/missionCompletedScene.ts
@@ -8,7 +8,7 @@ import createGenericScene from './genericScene';
  */
 const createMissionCompletedScene = (): IGameScene => {
   const title = 'MISSION COMPLETED';
-  const subtitle = 'All 404 cards were collected.';
+  const subtitle = 'Transmitting to central the 404 Cards found data.';
   const action = 'Press [x] to start the next mission.';
   const footer = `${Game.getInstance().missionCount} mission(s) completed`;
 
